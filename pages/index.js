6a1fe334830984b3a6d63f2646b0f0e1d2c6a976
Cesar Jimenez <jimgodw@gmail.com>
2020-12-18 22:31:55 -0600

    Add site modal and dashboard component creation

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import { Button, Heading, Text, Code } from '@chakra-ui/core';
+import { Button, Text, Code, Icon, Flex } from '@chakra-ui/core';
 
 import { useAuth } from '@/lib/auth';
 
@@ -7,22 +7,25 @@ const Home = () => {
   const auth = useAuth();
 
   return (
-    <div className="container">
+    <Flex
+      as="main"
+      direction="column"
+      align="center"
+      justify="center"
+      h="100vh"
+    >
       <Head>
         <title>Fast Feedback</title>
       </Head>
-      <main>
-        <Heading>Fast Feedback</Heading>
-        <Text>
-          Current user: <Code>{auth.user ? auth.user.email : 'None'}</Code>
-        </Text>
-        {auth.user ? (
-          <Button onClick={(e) => auth.signout()}>Sign Out</Button>
-        ) : (
-          <Button onClick={(e) => auth.signinWithGithub()}>Sign In</Button>
-        )}
-      </main>
-    </div>
+      <Icon color="black" name="logo" size="64px" />
+      {auth.user ? (
+        <Button onClick={(e) => auth.signout()}>Sign Out</Button>
+      ) : (
+        <Button mt={4} size="sm" onClick={(e) => auth.signinWithGithub()}>
+          Sign In
+        </Button>
+      )}
+    </Flex>
   );
 };
 
